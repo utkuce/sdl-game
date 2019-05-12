c0de4fa073d40f0da58522bf912138722411352d
Utku Çelik <utkuce@gmail.com>
2019-05-11 22:51:17 -0400

    Move webgl folder

--- WebGL/emscripten.mk
+++ WebGL/emscripten.mk
@@ -1,8 +1,8 @@
 CXX = em++ -std=c++11
 
 SOURCES = $(wildcard ../src/*.cpp)  
-SOURCES += $(wildcard ../../src/*.cpp)
-SOURCES += $(wildcard ../src/imgui/*.cpp)
+SOURCES += $(wildcard ../myengine/src/*.cpp)
+SOURCES += $(wildcard ../myengine/src/imgui/*.cpp)
 
 EMCC_FLAGS = -s USE_SDL=2 -s USE_WEBGL2=1 -s WASM=1 -s FULL_ES3=1 -s ALLOW_MEMORY_GROWTH=1
 EMCC_FLAGS += -s BINARYEN_TRAP_MODE=clamp --shell-file shell_minimal.html
